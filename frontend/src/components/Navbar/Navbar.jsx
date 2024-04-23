fca8afa2c2345260a9d86ed6407876bd509faef5
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-24 02:28:18 +0500

    updated Navbar
    -uploadated file loader so i can import svg (vite)
    -worked with circle svg
    -made navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
+import logo from "/src/assets/logo.svg";
 
 const Navbar = () => {
   return (
     <div>
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">
-            Home
-          </a>
+      <ul className="nav justify-content-between">
+        <li className="nav-item">
+          <img src={logo} style={{ padding: "10px" }} />
+          1zhanN
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Projects
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Contact
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">
-            Certificates
-          </a>
-        </li>
+        <div className="d-flex">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link " href="#">
+              Certificates
+            </a>
+          </li>
+        </div>
       </ul>
     </div>
   );
