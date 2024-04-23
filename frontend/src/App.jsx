4adecf97e7fabb240c6d063b4a1bf3f2cec5ee25
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-24 02:49:21 +0500

    centered

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,12 @@ import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <>
+    <div className="container col-sm-8">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      doloribus odio a cumque minus, mollitia modi iusto error obcaecati
+      provident, dolorum deserunt cum ullam corrupti enim sit veritatis,
+      adipisci aut?
+    </div>
+  );
 };
 
 export default Home;
