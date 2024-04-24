0cfb91912de8690779d6b56ab49e44879b5846ef
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-24 15:37:17 +0500

    Updated Card.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Home.css";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-      doloribus odio a cumque minus, mollitia modi iusto error obcaecati
-      provident, dolorum deserunt cum ullam corrupti enim sit veritatis,
-      adipisci aut?
+      <Card />
     </div>
   );
 };
